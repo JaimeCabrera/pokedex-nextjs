9df2200fc96443e6ff38310fb7f04aae69af5db4
Jaime Cabrera <jycabrerap@unl.edu.ec>
2022-04-09 16:10:55 -0500

    add sgi

--- utils/getPokemonInfo.ts
+++ utils/getPokemonInfo.ts
@@ -3,12 +3,16 @@ import { Pokemon } from "../interfaces";
 
 export const getPokemonInfo = async (nameOrId: string) => {
   // const { id } = params as { id: string };
-  const { data } = await pokemonApi.get<Pokemon>(`/pokemon/${nameOrId}`);
-  // filtrando solo la data que voy a usar en el json de las paginas stait
+  try {
+    const { data } = await pokemonApi.get<Pokemon>(`/pokemon/${nameOrId}`);
+    // filtrando solo la data que voy a usar en el json de las paginas stait
 
-  return {
-    id: data.id,
-    name: data.name,
-    sprites: data.sprites,
-  };
+    return {
+      id: data.id,
+      name: data.name,
+      sprites: data.sprites,
+    };
+  } catch (error) {
+    return null;
+  }
 };
